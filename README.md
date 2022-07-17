d5ab4ee166a3a9172edef4290a47533b950b82f4
Mohammed Ismail <ismail46h.shaikh@gmail.com>
2022-07-17 20:36:31 +0530

    update readme && Data Ingestion class Created

--- README.md
+++ README.md
@@ -1,126 +1,118 @@
-## ML_Project1
+Application url:
+[HousingPredictor](https://ml-regression-app.herokuapp.com/)
 
-### Software and Account Requirements:
+## Start Machine Learning project.
+
+### Software and account Requirement.
 
 1. [Github Account](https://github.com)
 2. [Heroku Account](https://dashboard.heroku.com/login)
-3. [VS-Code IDE](https://code.visualstudio.com/download)
-4. [Git-cli](https://git-scm.com/downloads).
-5. [Git-Command-Documentation](https://git-scm.com/docs/git)
+3. [VS Code IDE](https://code.visualstudio.com/download)
+4. [GIT cli](https://git-scm.com/downloads)
+5. [GIT Documentation](https://git-scm.com/docs/gittutorial)
 
 
-# Creating conda environment
-
-'''
+Creating conda environment
+```
 conda create -p venv python==3.7 -y
-'''
-
-'''
+```
+```
 conda activate venv/
-'''
-
-or
-
-'''
+```
+OR 
+```
 conda activate venv
-'''
+```
 
-'''
+```
 pip install -r requirements.txt
-'''
+```
 
-'''
-To Add Files to git
-'''
+To Add files to git
+```
+git add .
+```
 
-'''
-git add
-'''
+OR
+```
+git add <file_name>
+```
 
-or
+> Note: To ignore file or folder from git we can write name of file/folder in .gitignore file
 
-'''
-git add < file name >
-'''
-
-> Note :To Ignore file or folder from git we can write name of file/folder in.gitignore file
-
-#### To Check the git status
-
-'''
+To check the git status 
+```
 git status
-'''
-
-#### To check all version maintained by git
-
-'''
+```
+To check all version maintained by git
+```
 git log
-'''
+```
 
-#### To create version/commit all changes by git:
+To create version/commit all changes by git
+```
+git commit -m "message"
+```
 
-'''
-git commit-m "message"
-'''
-
-#### To Send version/changes to github
-
-'''
+To send version/changes to github
+```
 git push origin main
-'''
+```
 
-#### To Check remote url
-
-'''
+To check remote url 
+```
 git remote -v
-'''
+```
 
-#### To setup CI/CD pipeline in heroku we need 3 information
+To setup CI/CD pipeline in heroku we need 3 information
+1. HEROKU_EMAIL = anishyadav7045075175@gmail.com
+2. HEROKU_API_KEY = <>
+3. HEROKU_APP_NAME = ml-regression-app
 
-1. Heroku_Email = 'ismail46h.shaikh@gmail.com'.
-2. Heroku_API_Key = <>.
-3. Heroku_App_Name = machine-learning-practice.
-
-
-#### BUILD DOCKER IMAGE
-
-'''
+BUILD DOCKER IMAGE
+```
 docker build -t <image_name>:<tagname> .
-'''
-
+```
 > Note: Image name for docker must be lowercase
 
 
-#### To list docker image
-
-'''
+To list docker image
+```
 docker images
-'''
+```
 
-#### Run Docker image
+Run docker image
+```
+docker run -p 5000:5000 -e PORT=5000 f8c749e73678
+```
 
-'''
-docker run -p 5000:5000 -e PORT= 5000
-'''
-
-#### To Check Running container in docker
-
-'''
+To check running container in docker
+```
 docker ps
-'''
+```
 
-#### To stop docker container
-
-'''
+Tos stop docker conatiner
+```
 docker stop <container_id>
-'''
+```
 
 
-'''
+
+```
 python setup.py install
-'''
+```
 
-Install ipykernal
-'''
-pip install ipykernal
-'''
+
+Install ipykernel
+
+```
+pip install ipykernel
+```
+
+
+Data Drift:
+When your datset stats gets change we call it as data drift
+
+
+
+## Write a function to get training file path from artifact dir
--- housing/entity/config_entity.py
+++ housing/entity/config_entity.py
@@ -2,29 +2,84 @@ from collections import namedtuple
 from unicodedata import name
 
 DataIngestionConfig = namedtuple("DataIngestionConfig", 
-["dataset_download_url",
-"tgz_download_dir",
-"raw_data_dir",
-"ingested_train_dir",
-"ingested_test_dir"])
-
-DataValidationConfig = namedtuple("DataValidationConfig",["schema_file_path"])
-
-DataTransformationConfig = namedtuple("DataTransformationConfig",["add_bedroom_per_room",
-                                                                    "transformed_train_dir",
-                                                                    "transformed_test_dir",
-                                                                    "preprocessed_object_file_path"])
+        [
+        "dataset_download_url",
+        "tgz_download_dir",
+        "raw_data_dir",
+        "ingested_train_dir",
+        "ingested_test_dir"
+        ])
+# -------------------------------------------------------------------------------------------
 
 
-ModelTrainerConfig = namedtuple("ModelTrainerConfig",["trained_model_file_path","base_accuracy"])
-
-ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",["model_evaluation_file_path","time_stamp"])
-
-ModelPusherConfig = namedtuple("ModelPusherConfig",["export_dir_path"])
-
-TrainingPipelineConfig = namedtuple("TrainingPipelineConfig",["artifact_dir"])
+DataValidationConfig = namedtuple("DataValidationConfig",
+        ["schema_file_path"])
+# schema_file_path : How many number of column and ehat are there datatype 
+# -------------------------------------------------------------------------------------------
 
 
+DataTransformationConfig = namedtuple("DataTransformationConfig",
+        [
+        "add_bedroom_per_room",
+        "transformed_train_dir",
+        "transformed_test_dir",
+        "preprocessed_object_file_path"
+        ])
 
+'''
+DataTransformationConfig        : For Feature engineering
+add_bedroom_per_room (Optional) : dataset this collumn is not available then we will add this column as (True)
+transformed_train_dir           : preprocessing apply and then transform dataset location
+transformed_test_dir            : after apply feature enginnering where we will apply after transformation we will do in this location 
+preprocessed_object_file_path   : pickle file created of abouve info and  stored in this file
+'''
+# -------------------------------------------------------------------------------------
+
+
+ModelTrainerConfig = namedtuple("ModelTrainerConfig",
+        [
+        "trained_model_file_path",
+        "base_accuracy"
+        ])
+'''
+ModelTrainerConfig      : 
+trained_model_file_path : after trainning model export in pickle file that location specify through this 
+base_accuracy           : minimum criteria for accuracy 
+'''
+# -------------------------------------------------------------------------------------
+
+
+ModelEvaluationConfig = namedtuple("ModelEvaluationConfig",
+        [
+        "model_evaluation_file_path",
+        "time_stamp"
+        ])
+'''
+ModelEvaluationConfig      :
+model_evaluation_file_path : keep information about all model which is in production, 
+                                use test data, and compare models
+time_stamp                 :  just to stored activity of comparing at what time happen
+
+'''
+# -------------------------------------------------------------------------------------
+
+
+ModelPusherConfig = namedtuple("ModelPusherConfig",
+        ["export_dir_path"])
+
+'''
+ModelPusherConfig
+export_dir_path     : where i will export my train model
+'''
+# -------------------------------------------------------------------------------------
+
+
+TrainingPipelineConfig = namedtuple("TrainingPipelineConfig",
+        ["artifact_dir"])
+
+'''
+TrainingPipelineConfig
+artifact_dir
+'''
 
 
